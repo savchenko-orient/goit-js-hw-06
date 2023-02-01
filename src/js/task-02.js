e7908b3f49d5376e3223e971d34e552892edf9d7
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-01 19:06:15 +0200

    task 2 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+ingredients.forEach(elem => {
+  const listItem = document.createElement('li');
+
+  listItem.classList.add('item');
+  listItem.innerText = `${elem}`;
+  listEl.append(listItem);
+
+});
