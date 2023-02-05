cce08a94933d3d6034e1f2414911a74847ad9368
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-05 14:47:02 +0200

    task 9 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const bodyEl = document.querySelector('body'),
+  btn = document.querySelector('.change-color'),
+  spanEl = document.querySelector('.color');
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const changeColorHandler = () => {
+  spanEl.innerText = `${getRandomHexColor()}`
+  bodyEl.style.backgroundColor = spanEl.innerText;
 }
+
+btn.addEventListener('click', changeColorHandler)
