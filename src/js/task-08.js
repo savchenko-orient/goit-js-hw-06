8296ea7916bfaaef5a23d4324fa2151f054c98cf
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-05 14:35:59 +0200

    task 8 completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+let formData = {};
+
+const handlerSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.elements.email.value && form.elements.password.value) {
+        formData.email = form.elements.email.value;
+        formData.password = form.elements.password.value;
+
+        console.log('formData: ', formData);
+        form.reset()
+
+        return
+    }
+
+    alert('Всі поля повинні бути заповнені');
+}
+
+form.addEventListener('submit', handlerSubmit)
