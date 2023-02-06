14b0d13c6f6fc006d4ab08d4fc6af3542234e786
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-06 17:46:36 +0200

    task 10 complete

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,34 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes')
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const createBoxes = amount => {
+  let newBoxes = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    let box = document.createElement('div');
+
+    const boxSize = 30 + (10 * i);
+
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    box.style.height = `${boxSize}px`;
+    box.style.width = `${boxSize}px`;
+
+    newBoxes.push(box);
+  }
+
+  return newBoxes;
 }
+
+const destroyBoxes = () => boxes.innerHTML = '';
+
+destroyBtn.addEventListener('click', destroyBoxes)
+
+createBtn.addEventListener('click', () => {
+  let newBoxes = createBoxes(input.value);
+
+  boxes.append(...newBoxes)
+})
