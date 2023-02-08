86d5ba9b07bcb095c4ae8725ecf1f363b7bb8e87
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-08 11:30:27 +0200

    fix task 4

--- js/task-04.js
+++ js/task-04.js
@@ -2,13 +2,11 @@ const currentValue = document.querySelector('#value'),
     decrementBtn = document.querySelector('[data-action="decrement"]'),
     incrementBtn = document.querySelector('[data-action="increment"]');
 
-let count = 0
-
-const handlerIncrement = () => currentValue.innerText = count += 1;
+const handlerIncrement = () => currentValue.innerText = Number(currentValue.innerText) + 1;
 
 const handlerDecrement = () => {
     Number(currentValue.innerText) > 0
-        ? currentValue.innerText = count -= 1
+        ? currentValue.innerText = currentValue.innerText - 1
         : currentValue;
 }
 
