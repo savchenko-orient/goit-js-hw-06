ca3b586d765fe5274a1907bdf06d6feb12b3e938
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-08 13:17:37 +0200

    6

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,10 @@ const handlerValidationInput = () => {
 
     if (isValidInput) {
         input.classList.add("valid")
-        input.classList.remove('invalid')
+        input?.classList.remove('invalid')
     } else {
         input.classList.add("invalid")
-        input.classList.remove('valid')
+        input?.classList.remove('valid')
     }
 }
 
